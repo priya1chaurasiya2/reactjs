800ff8d1ac1898d7c47ef129981624f661257104
priya1chaurasiya2 <priya2017chaurasiya@gmail.com>
2018-07-05 15:25:29 +0530

    life cycle methods

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,8 @@ class App extends React.Component {
    constructor(){
        super();
        this.state={
-         homeLink:"Home"  
+         homeLink:"Home",
+         homeMounted:true
        };
    } 
 
@@ -20,8 +21,26 @@ class App extends React.Component {
            homeLink:newName
        });
     }
+
+    onChangeHomeMounted(){
+        this.setState({
+            homeMounted:!this.state.homeMounted
+        });
+    }
     render() {
-        
+        let homeCmp="";
+        if(this.state.homeMounted){
+            homeCmp= (<Home 
+            name="Max" 
+            initialAge={27} 
+            greet={this.onGreet}
+            changeLink={this.onChangeLinkName.bind(this)}
+            initialLinkName={this.state.homeLink}
+            />
+            );
+
+        }
+    
         return (<div className="container">
             <div className="row">
                 <div className=".col-xs-10 .col-xs-offset-1">
@@ -30,14 +49,13 @@ class App extends React.Component {
             </div>
             <div className="row">
                 <div className=".col-xs-10 .col-xs-offset-1">
-                   <Home 
-                   name="Max" 
-                   initialAge={27} 
-                   greet={this.onGreet}
-                   changeLink={this.onChangeLinkName.bind(this)}
-                   initialLinkName={this.state.homeLink}
-                   />
-                 
+                  {homeCmp}
+                
+                </div>
+            </div>
+            <div className="row">
+                <div className=".col-xs-10 .col-xs-offset-1">
+                <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount Home Component</button>
                 
                 </div>
             </div>
